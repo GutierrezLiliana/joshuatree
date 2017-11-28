af415242ca0ca07b592f86c11f11de669fc54c6f
GutierrezLiliana <liliana_gutierrez@davincischools.org>
2017-11-28 13:10:12 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// JavaScript is capitalized using "camel case": https://en.wikipedia.org/wiki/Camel_case
+
+// Once you've identified the elements you want to change, you can use the "classList" (to change classes) or "innerHTML" (to change the text):
+// classList = https://www.w3schools.com/jsref/prop_element_classlist.asp
+// innerHTML = https://www.w3schools.com/jsref/prop_html_innerhtml.asp
+
+function resetAll(){
+
+}
+
+function showText(elem){
+	elem.innerHTML = "JavaScriptSorcery";
+}
+
+function toggleBlueBackground(elem){
+	document.getElementById("jtree-background").classList.toggle("tan");
+}
